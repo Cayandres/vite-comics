8e73dd0e5b0db7489c838fe2c78a3e6e84a58587
Andres Cayambe <andrescayambe99@hotmail.com>
2023-04-27 19:36:57 +0200

    [add]header

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,23 @@
-
 <script>
+import Header from "./components/Header.vue"
 export default {
+  name: "App",
   
+  components:{
+    Header,
+  }
 }
 </script>
 
 <template>
-  
+  <div class="container">
+    <Header/>
+    <h1>ciao</h1>
+  </div>
 </template>
 
-<style>
+<style lang="scss">
+
+@use "./scss/main.scss";
 
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,59 @@
+<script>
+export default {
+  name:'header'
+  
+}
+</script>
+
+<template>
+  <header>
+    <div>
+      <img src="../assets/img/dc-logo.png" alt="">
+    </div>
+    <div>
+      <nav>
+        <ul>
+          <li><a href="#">1</a></li>
+          <li><a href="#">1</a></li>
+          <li><a href="#">1</a></li>
+          <li><a href="#">1</a></li>
+          <li><a href="#">1</a></li>
+          <li><a href="#">1</a></li>
+          <li><a href="#">1</a></li>
+          <li><a href="#">1</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
+
+
+<style lang="scss" scoped>
+  @use "../scss/general/variables" as*;
+  @use "../scss/general/mixin" as*;
+
+  header{
+    @include beetweenAround("beetween");
+    align-items: center;
+    ul{
+      display: flex;
+      li{
+        list-style: none;
+        margin: 20px;
+        padding: 20px;
+        a{
+          text-decoration: none;
+          color: black;
+            &:hover{
+              color: $primary-color;
+            }
+        }
+      }
+    }
+  }
+ 
+
+
+
+
+</style>
